11139cf62f650a8b981268ecff5a7c59b362f01d
Thiago Spindola <thiago.spindola@aapvr.com.br>
2018-10-15 17:58:36 -0300

    Grafico e Consulta p dados

--- Hops/CERVEJEIRO.sql
+++ Hops/CERVEJEIRO.sql
@@ -1,7 +1,7 @@
 -- MySQL Administrator dump 1.4
 --
 -- ------------------------------------------------------
--- Server version	5.0.96-community-nt
+-- Server version	5.0.96-community-nt-log
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -22,6 +22,42 @@ CREATE DATABASE IF NOT EXISTS cervejeiro;
 USE cervejeiro;
 
 --
+-- Temporary table structure for view `lupulo_oleo_ext`
+--
+DROP TABLE IF EXISTS `lupulo_oleo_ext`;
+DROP VIEW IF EXISTS `lupulo_oleo_ext`;
+CREATE TABLE `lupulo_oleo_ext` (
+  `idLupOleo` int(10) unsigned,
+  `idLup` int(10) unsigned,
+  `idOleo` int(10) unsigned,
+  `quantOleo` decimal(10,2),
+  `Myrcene` decimal(10,2),
+  `Humulene` decimal(10,2),
+  `B-Pinene` decimal(10,2),
+  `Caryophyllene` decimal(10,2),
+  `Farnesene` decimal(10,2),
+  `Geraniol` decimal(10,2),
+  `Linalool` decimal(10,2)
+);
+
+--
+-- Temporary table structure for view `lupulo_oleo_ext_pivot`
+--
+DROP TABLE IF EXISTS `lupulo_oleo_ext_pivot`;
+DROP VIEW IF EXISTS `lupulo_oleo_ext_pivot`;
+CREATE TABLE `lupulo_oleo_ext_pivot` (
+  `idlup` int(10) unsigned,
+  `Myrcene` decimal(32,2),
+  `Humulene` decimal(32,2),
+  `B-Pinene` decimal(32,2),
+  `Caryophyllene` decimal(32,2),
+  `Farnesene` decimal(32,2),
+  `Geraniol` decimal(32,2),
+  `Linalool` decimal(32,2),
+  `Outros` decimal(39,2)
+);
+
+--
 -- Definition of table `lupulo`
 --
 
@@ -37,7 +73,7 @@ CREATE TABLE `lupulo` (
   `estilosLup` text,
   `perfilAromaLup` text,
   PRIMARY KEY  (`idLup`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lupulo`
@@ -48,7 +84,8 @@ INSERT INTO `lupulo` (`idLup`,`nomeLup`,`paisLup`,`tipoLup`,`AALup`,`safraLup`,`
  (1,'Citra','EUA','Amargor e Aroma','15.00',NULL,'4.00','India Pale Ale, Ales Americanas, Amber','Cítrico, toranja, limão, frutas tropicais, harsh'),
  (2,'Centennial','EUA','Amargor e Aroma','10.00',NULL,'4.00','Extra Special Bitter, Barley Wine, Imperial Stout, India Pale Ale, Pale Ale','Terroso e floral com um elemento cítrico'),
  (3,'Amarillo','EUA','Aroma','9.50',NULL,'6.50','American Pale Ale, India Pale Ale, American Wheat, Bitter, Wheat Beer, Red Ale, ESB','Laranja e cítrico'),
- (4,'Azacca','EUA','Amargor e Aroma','15.00',NULL,'5.00','India Pale Ale','Aromas de frutas tropicais e cítrico. Notas de manga, abacaxi, tangerina e pinho.');
+ (4,'Azacca','EUA','Amargor e Aroma','15.00',NULL,'5.00','India Pale Ale','Aromas de frutas tropicais e cítrico. Notas de manga, abacaxi, tangerina e pinho.'),
+ (5,'Bravo','EUA','Amargor','15.50',NULL,'4.00','India Pale Ale, American Pale Ale, Extra Special Bitter','Picante, terroso e levemente floral');
 /*!40000 ALTER TABLE `lupulo` ENABLE KEYS */;
 
 
@@ -67,7 +104,7 @@ CREATE TABLE `lupulo_oleo` (
   KEY `FK_lupulo_oleo_idOleo` (`idOleo`),
   CONSTRAINT `FK_lupulo_oleo_idLup` FOREIGN KEY (`idLup`) REFERENCES `lupulo` (`idLup`),
   CONSTRAINT `FK_lupulo_oleo_idOleo` FOREIGN KEY (`idOleo`) REFERENCES `oleo` (`idOleo`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lupulo_oleo`
@@ -95,7 +132,12 @@ INSERT INTO `lupulo_oleo` (`idLupOleo`,`idLup`,`idOleo`,`quantOleo`) VALUES
  (18,3,4,'6.53'),
  (19,3,5,'6.39'),
  (20,3,6,'0.16'),
- (21,3,7,'0.61');
+ (21,3,7,'0.61'),
+ (22,1,8,'0.11'),
+ (23,1,9,'0.89'),
+ (24,1,10,'0.23'),
+ (25,1,11,'0.86'),
+ (26,1,12,'0.41');
 /*!40000 ALTER TABLE `lupulo_oleo` ENABLE KEYS */;
 
 
@@ -109,7 +151,7 @@ CREATE TABLE `oleo` (
   `nomeOleo` varchar(70) NOT NULL,
   `caracteristicaOleo` text NOT NULL,
   PRIMARY KEY  (`idOleo`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `oleo`
@@ -119,13 +161,60 @@ CREATE TABLE `oleo` (
 INSERT INTO `oleo` (`idOleo`,`nomeOleo`,`caracteristicaOleo`) VALUES 
  (1,'Myrcene','Resina, pinho, herbal, picante, cítrico e floral.'),
  (2,'Humulene','Floral, herbal e condimentado.'),
- (3,'B-Pinene','Picante, Pinho'),
+ (3,'B_Pinene','Picante, Pinho'),
  (4,'Caryophyllene','Madeira'),
  (5,'Farnesene','Floral'),
  (6,'Geraniol','Floral, doce, rosa'),
- (7,'Linalool','Floral, laranja');
+ (7,'Linalool','Floral, laranja'),
+ (8,'A_Pinene','A'),
+ (9,'II_MethylButyl','A'),
+ (10,'Limonene','A'),
+ (11,'Citral','A'),
+ (12,'Geranyl_Acet1','A'),
+ (13,'Geranyl_Acet2','A'),
+ (14,'Citronellol','A');
 /*!40000 ALTER TABLE `oleo` ENABLE KEYS */;
 
+
+--
+-- Definition of table `tbproj_cenario`
+--
+
+DROP TABLE IF EXISTS `tbproj_cenario`;
+CREATE TABLE `tbproj_cenario` (
+  `projCenProj_Id` int(11) default NULL,
+  `projCenNome` varchar(20) default NULL,
+  `valor` float(4,2) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbproj_cenario`
+--
+
+/*!40000 ALTER TABLE `tbproj_cenario` DISABLE KEYS */;
+INSERT INTO `tbproj_cenario` (`projCenProj_Id`,`projCenNome`,`valor`) VALUES 
+ (1,'Cenário I',1.00),
+ (2,'Cenário II',2.00),
+ (3,'Cenário III',2.40),
+ (4,'Cenário III',5.00);
+/*!40000 ALTER TABLE `tbproj_cenario` ENABLE KEYS */;
+
+
+--
+-- Definition of view `lupulo_oleo_ext`
+--
+
+DROP TABLE IF EXISTS `lupulo_oleo_ext`;
+DROP VIEW IF EXISTS `lupulo_oleo_ext`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `lupulo_oleo_ext` AS (select `lo`.`idLupOleo` AS `idLupOleo`,`lo`.`idLup` AS `idLup`,`lo`.`idOleo` AS `idOleo`,`lo`.`quantOleo` AS `quantOleo`,(case when (`lo`.`idOleo` = 1) then `lo`.`quantOleo` end) AS `Myrcene`,(case when (`lo`.`idOleo` = 2) then `lo`.`quantOleo` end) AS `Humulene`,(case when (`lo`.`idOleo` = 3) then `lo`.`quantOleo` end) AS `B-Pinene`,(case when (`lo`.`idOleo` = 4) then `lo`.`quantOleo` end) AS `Caryophyllene`,(case when (`lo`.`idOleo` = 5) then `lo`.`quantOleo` end) AS `Farnesene`,(case when (`lo`.`idOleo` = 6) then `lo`.`quantOleo` end) AS `Geraniol`,(case when (`lo`.`idOleo` = 7) then `lo`.`quantOleo` end) AS `Linalool` from `lupulo_oleo` `lo`);
+
+--
+-- Definition of view `lupulo_oleo_ext_pivot`
+--
+
+DROP TABLE IF EXISTS `lupulo_oleo_ext_pivot`;
+DROP VIEW IF EXISTS `lupulo_oleo_ext_pivot`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `lupulo_oleo_ext_pivot` AS (select `lupulo_oleo_ext`.`idLup` AS `idlup`,coalesce(sum(`lupulo_oleo_ext`.`Myrcene`),0) AS `Myrcene`,coalesce(sum(`lupulo_oleo_ext`.`Humulene`),0) AS `Humulene`,coalesce(sum(`lupulo_oleo_ext`.`B-Pinene`),0) AS `B-Pinene`,coalesce(sum(`lupulo_oleo_ext`.`Caryophyllene`),0) AS `Caryophyllene`,coalesce(sum(`lupulo_oleo_ext`.`Farnesene`),0) AS `Farnesene`,coalesce(sum(`lupulo_oleo_ext`.`Geraniol`),0) AS `Geraniol`,coalesce(sum(`lupulo_oleo_ext`.`Linalool`),0) AS `Linalool`,(((((((100 - sum(`lupulo_oleo_ext`.`Myrcene`)) - sum(`lupulo_oleo_ext`.`Humulene`)) - sum(`lupulo_oleo_ext`.`B-Pinene`)) - sum(`lupulo_oleo_ext`.`Caryophyllene`)) - sum(`lupulo_oleo_ext`.`Farnesene`)) - sum(`lupulo_oleo_ext`.`Geraniol`)) - sum(`lupulo_oleo_ext`.`Linalool`)) AS `Outros` from `lupulo_oleo_ext` group by `lupulo_oleo_ext`.`idLup`);
 
 
 
--- Hops/index.php
+++ Hops/index.php
@@ -18,6 +18,8 @@ $listaLup = $lupulos->getListaLupulo();
         <link rel="stylesheet" href="css/bootstrap.css">
         <link rel="icon" type="image/png" href="imagens/hop_edit.png">
         <title>Lupulando - Ferramenta Para Cervejeiros Caseiros</title>
+        <script src="js/jquery-3.3.1.js"></script>
+        <script src="js/Chart.bundle.js"></script>
     </head>
     <body>
         <nav class="navbar navbar-light" style="background-color: #32b344;">
@@ -62,8 +64,49 @@ $listaLup = $lupulos->getListaLupulo();
                 </div>
                 <?php } ?>
             </fieldset>
+            <div class="container-fluid w-50">
+                <canvas id="myChart" width="400" height="400"></canvas>
+                <script>
+                    var ctx = document.getElementById("myChart");
+                    var myChart = new Chart(ctx, {
+                        type: 'radar',
+                        data: {
+                            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                            datasets: [{
+                                label: '# of Votes',
+                                data: [12, 19, 3, 5, 2, 3],
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 0.2)',
+                                    'rgba(54, 162, 235, 0.2)',
+                                    'rgba(255, 206, 86, 0.2)',
+                                    'rgba(75, 192, 192, 0.2)',
+                                    'rgba(153, 102, 255, 0.2)',
+                                    'rgba(255, 159, 64, 0.2)'
+                                ],
+                                borderColor: [
+                                    'rgba(255,99,132,1)',
+                                    'rgba(54, 162, 235, 1)',
+                                    'rgba(255, 206, 86, 1)',
+                                    'rgba(75, 192, 192, 1)',
+                                    'rgba(153, 102, 255, 1)',
+                                    'rgba(255, 159, 64, 1)'
+                                ],
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        beginAtZero:true
+                                    }
+                                }]
+                            }
+                        }
+                    });
+                </script>
+            </div>
         </div>
-        <script src="js/jquery-3.3.1.js"></script>
         <script src="js/bootstrap.js"></script>
     </body>
 </html>
